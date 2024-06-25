38dc69ac32420d819a7a2e27f9a1ee4df1f04258
Marvin Boyi <boyijayjay@gmail.com>
2024-06-25 17:41:46 -0400

    Added API to POST/CREATE a New Warehouse

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config'; 
+import 'dotenv/config';
 import cors from 'cors';
 import warehouse from "./routes/warehouse.js";
 import inventory from "./routes/inventory.js";
@@ -13,10 +13,13 @@ const PORT = process.env.PORT || 8080;
 // Initialize knex with the database configuration.
 const db = knex(dbConfig);
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 // All routes
 app.use(cors()); 
 app.use("/warehouse", warehouse);
-app.use("/inventories", inventory);
+app.use("/inventory", inventory);
 
 app.get("/", (req, res) => {
     res.status(200).send("Testing Request Successful");
