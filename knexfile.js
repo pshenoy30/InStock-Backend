722538b97efc6caba1d4a2bea3b09d4a83275d12
Marvin Boyi <boyijayjay@gmail.com>
2024-06-25 00:01:00 -0400

    Set up a knex configuration file to connect to db instock

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Import dotenv to process environment variables from `.env` file.
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConfig = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  }
+};
+
+export default dbConfig;
