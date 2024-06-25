18872fd53026b249e7805d742443f08970191989
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-25 22:46:34 +0400

    Added the controllers folder and warehouse-controller in it

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,18 +1,8 @@
 import express from 'express';
-import "dotenv";
-import initKnex from "knex";
-import configuration from "../knexfile.js";
+import * as warehouseController from '../controllers/warehouse-controller.js'
 
-const knex = initKnex(configuration);
 const router = express.Router();
 
-router.get("/", async (req,res) => {
-    try{
-        const data = await knex("warehouse");
-        res.status(200).json(data);
-    } catch (err){
-        res.status(400).send(`Error in retrieving warehouse details: ${err}`)
-    }
-})
+router.route('/').get(warehouseController.warehouseIndex);
 
 export default router;
