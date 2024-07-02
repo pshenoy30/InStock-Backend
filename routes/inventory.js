59c5b1c0c8d3fcd9e77e607979078d400caec734
Marvin Boyi <105250318+therealboyi@users.noreply.github.com>
2024-07-02 18:41:11 -0500

    Revert "Production Launch"

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,15 +1,9 @@
 import express from 'express';
-import * as inventoryController from "../controllers/inventories-controller.js";
+import "dotenv";
 
 const router = express.Router();
 
-router.route('/')
-    .get(inventoryController.inventoryIndex)
-    .post(inventoryController.addInventoryItem);
 
-router.route('/:id')
-    .get(inventoryController.inventoryItemBasedOnId)
-    .put(inventoryController.updateInventoryItem)
-    .delete(inventoryController.deleteInventoryItem);
+
 
 export default router;
